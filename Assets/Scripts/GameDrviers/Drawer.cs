3e38afc8a0763fa5515a64598da7b0e1c372460e
crass_sandwich <christopherjanidlo@gmail.com>
2019-03-31 23:10:38 -0400

    Fix two-long lists

--- Assets/Scripts/GameDrviers/Drawer.cs
+++ Assets/Scripts/GameDrviers/Drawer.cs
@@ -199,11 +199,13 @@ public class Drawer : MonoBehaviour
         
         if (list.Count == 1) return output + ".";
 
+        if (list.Count == 2) return $"{output} and {list[1]}.";
+
         for (int i = 1; i < list.Count - 1; i++)
         {
             output += $", {list[i]}";
         }
         
-        return output + $", and {list[list.Count - 1]}.";
+        return $"{output}, and {list[list.Count - 1]}.";
     }
 }
