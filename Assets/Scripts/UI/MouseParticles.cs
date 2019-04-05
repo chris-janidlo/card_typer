33b9cee465fea784e78edee62427f901ef11a0ff
crassSandwich <christopherjanidlo@gmail.com>
2019-04-05 12:58:50 -0400

    Mouse particles

---
+++ Assets/Scripts/UI/MouseParticles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using crass;
+
+public class MouseParticles : MonoBehaviour
+{
+    public RectTransform Canvas;
+
+    void Update ()
+    {
+        Vector3 pos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(Canvas, Input.mousePosition, CameraCache.Main, out pos);
+        transform.position = pos;   
+    }
+}
