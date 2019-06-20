4d586c85198c975dabaf767ffe93b7a45653731d
crass_sandwich <christopherjanidlo@gmail.com>
2019-06-20 18:50:24 -0400

    Create barebones server program

--- server/Program.cs
+++ server/Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
+using System.Threading;
+using LiteNetLib;
+using LiteNetLib.Utils;
 
 class Program
 {
+    public static int Port = 9050;
+
     static void Main (string[] args)
     {
-        Console.WriteLine("Hello World!");
+        EventBasedNetListener listener = new EventBasedNetListener();
+        NetManager server = new NetManager(listener);
+        server.Start(Port);
+
+        listener.ConnectionRequestEvent += request => request.Accept();
+
+        listener.PeerConnectedEvent += peer =>
+        {
+            Console.WriteLine("We got connection: {0}", peer.EndPoint);
+            NetDataWriter writer = new NetDataWriter();
+            writer.Put("Hello client!");
+            peer.Send(writer, DeliveryMethod.ReliableOrdered);
+        };
+
+        Console.WriteLine($"Server started on port {Port}. Press any key to stop it.");
+
+        while (!Console.KeyAvailable)
+        {
+            server.PollEvents();
+            Thread.Sleep(15);
+        }
+        Console.ReadKey(true);
+
+        server.Stop();
+
+        Console.WriteLine("Server stopped.");
     }
 }
