343eb34c31b25263fdfdc23cc49407fa6833b2e2
crassSandwich <christopherjanidlo@gmail.com>
2019-06-23 17:56:12 -0400

    Change LocalMatchManager to a simple singleton container

---
+++ client/Assets/Scripts/MatchMechanics/ManagerContainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using CTShared;
+using crass;
+
+public class ManagerContainer : Singleton<ManagerContainer>
+{
+    [SerializeField]
+    private TextAsset player1Deck, player2Deck;
+
+    public static MatchManager Manager => Instance.manager;
+    MatchManager manager;
+
+    void Awake ()
+    {
+        if (SingletonGetInstance() != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            SingletonSetInstance(this, false);
+            DontDestroyOnLoad(gameObject);
+
+            manager = new MatchManager(player1Deck.text, player2Deck.text);
+            
+            manager.Player1.OnDeath += () => SceneManager.LoadScene("Loss");
+            manager.Player2.OnDeath += () => SceneManager.LoadScene("Victory");
+        }
+    }
+}
