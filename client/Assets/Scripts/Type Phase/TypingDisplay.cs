15e3e68ad4643ba524fb510ad298dbd768581932
crass_sandwich <christopherjanidlo@gmail.com>
2019-06-24 19:25:33 -0400

    Add typing feedback UI

---
+++ client/Assets/Scripts/Type Phase/TypingDisplay.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CTShared;
+using TMPro;
+
+public class TypingDisplay : MonoBehaviour
+{
+	public TagPair BadLetterTag;
+	public TextMeshProUGUI ProgressDisplay;
+
+	List<Card> play;
+
+	string progress
+	{
+		get => ProgressDisplay.text;
+		set => ProgressDisplay.text = value;
+	}
+
+	void Start ()
+	{
+		var mgr = MatchEvents.Instance;
+
+		mgr.OnTypePhaseStart.AddListener(startPhase);
+		mgr.OnTypePhaseEnd.AddListener(endPhase);
+	}
+
+	public void SetPlay (List<Card> _play)
+	{
+		play = _play;
+	}
+
+	void startPhase ()
+	{
+		progress = "";
+		ProgressDisplay.enabled = true;
+	}
+
+	void endPhase ()
+	{
+		ProgressDisplay.enabled = false;
+	}
+
+	public void Type (KeyboardKey key)
+	{
+		switch (key)
+		{
+			case KeyboardKey.Return:
+			case KeyboardKey.Space:
+				progress = "";
+				break;
+			
+			case KeyboardKey.Backspace:
+				deleteLetter();
+				break;
+			
+			default:
+				addLetter(key.ToChar());
+				break;
+		}
+	}
+
+	void deleteLetter ()
+	{
+		int delLen = 1;
+		if (progress[progress.Length - 1] == '>')
+		{
+			// 'c' here is totally arbitrary - just getting the length of one wrapped character
+			delLen = BadLetterTag.Wrap('c').Length;
+		}
+
+		progress = progress.Substring(0, progress.Length - delLen);
+	}
+
+	void addLetter (char letter)
+	{
+		var next = progress + letter;
+
+		if (play.Any(c => c.Word.StartsWith(next)))
+		{
+			progress = next;
+		}
+		else
+		{
+			next = BadLetterTag.Wrap(letter);
+			progress += next;
+		}
+	}
+}
