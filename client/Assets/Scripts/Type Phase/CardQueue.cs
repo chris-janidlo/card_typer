4393cbaacd0053ca9ad2866f6e413daeeeddf00d
crass_sandwich <christopherjanidlo@gmail.com>
2019-06-24 19:52:09 -0400

    Make CardQueue class

---
+++ client/Assets/Scripts/Type Phase/CardQueue.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CTShared;
+using TMPro;
+
+public class CardQueue : MonoBehaviour
+{
+	public TextMeshProUGUI DisplayPrefab;
+	public RectTransform DisplayContainer;
+
+	List<TextMeshProUGUI> displays = new List<TextMeshProUGUI>();
+
+	public void Initialize (MatchEvents.AgentEvents agent)
+	{
+		agent.OnAttemptedCast.AddListener(castCard);
+		MatchEvents.Instance.OnTypePhaseEnd.AddListener(clearQueue);
+	}
+
+	public void SetPlay (List<Card> play)
+	{
+		foreach (var card in play)
+		{
+			var display = Instantiate(DisplayPrefab);
+			display.transform.SetParent(DisplayContainer, false);
+			displays.Add(display);
+		}
+	}
+
+	void castCard (string name)
+	{
+		if (name.Equals("")) return;
+
+		var display = displays.First(d => d.text.Equals(name));
+		displays.Remove(display);
+		Destroy(display.gameObject);
+	}
+
+	void clearQueue ()
+	{
+		foreach (var disp in displays)
+		{
+			Destroy(disp.gameObject);
+		}
+
+		displays.Clear();
+	}
+}
