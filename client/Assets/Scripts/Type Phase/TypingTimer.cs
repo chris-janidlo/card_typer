eee4c76e913abfad395f269b5c90eb5f53fae4e4
crass_sandwich <christopherjanidlo@gmail.com>
2019-06-29 01:51:56 -0400

    Add Typing Timer

---
+++ client/Assets/Scripts/Type Phase/TypingTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CTShared;
+using TMPro;
+
+public class TypingTimer : MonoBehaviour
+{
+    public TextMeshProUGUI Display;
+
+    bool inPreTypePhase, inTypePhase;
+    MatchManager manager;
+
+    void Start ()
+    {
+        manager = ManagerContainer.Manager;
+
+        manager.OnPreTypePhaseStart += () => inPreTypePhase = true;
+        manager.OnTypePhaseStart += () => {
+            inPreTypePhase = false;
+            inTypePhase = true;
+        };
+        manager.OnTypePhaseEnd += () => inTypePhase = false;
+    }
+
+    void Update ()
+    {
+        if (inPreTypePhase)
+        {
+            Display.text = Mathf.Ceil(manager.CountdownTimer).ToString();
+        }
+        else if (inTypePhase)
+        {
+            Display.text = Mathf.Ceil(manager.TypingTimer).ToString();
+        }
+        else
+        {
+            Display.text = "";
+        }
+    }
+}
