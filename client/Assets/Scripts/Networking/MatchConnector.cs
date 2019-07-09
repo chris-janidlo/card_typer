e4e7b4509335f598702c4993f029311bb8203dd8
crass_sandwich <christopherjanidlo@gmail.com>
2019-07-09 13:44:49 -0400

    Client connection code

---
+++ client/Assets/Scripts/Networking/MatchConnector.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LiteNetLib;
+using CTShared.Networking;
+
+public class MatchConnector : MonoBehaviour
+{
+    public string IP;
+    public TextAsset DeckAsset;
+
+    NetManager client;
+
+    void Start ()
+    {
+        EventBasedNetListener listener = new EventBasedNetListener();
+        client = new NetManager(listener);
+
+        listener.NetworkReceiveEvent += handlePacket;
+
+        client.Start();
+        client.Connect(IP, NetworkConstants.ServerPort, NetworkConstants.VersionNumberConnectionKey);
+    }
+
+    void Update ()
+    {
+        client.PollEvents();
+    }
+
+    void OnDestroy ()
+    {
+        client.Stop();
+    }
+
+    void handlePacket (NetPeer peer, NetPacketReader reader, DeliveryMethod deliveryMethod)
+    {
+        PacketType type = (PacketType) reader.GetInt();
+
+        switch (type)
+        {
+            case PacketType.ServerReadyToReceiveDeck:
+                Debug.Log("got server ready to receive deck");
+                break;
+            
+            default:
+                Debug.LogError($"unexpected packet type {type}");
+                break;
+        }
+
+        reader.Recycle();
+    }
+}
